9aeee4fb92cd904374c7122410e1194ff7f55dd8
Felix Anducho <felix.anducho74@gmail.com>
2020-05-22 19:30:51 -0500

    Add files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # testingReactApps
+
+Part 5 of FullStackOpen, testing react applications.https://fullstackopen.com/en/part5/login_in_frontend. Connect backend from Part 4 with this project https://github.com/Pleopleq/testingExpress.
---
+++ src/App.js
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login';
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if(loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const handleLogin = async ( event ) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log('wrong credentials')
+    }
+  }
+
+  const handleLogOut = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    window.location.reload()
+  }
+
+    
+      if (user === null) { 
+      return ( 
+      <div> 
+        <h2>log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+              username
+              <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({ target }) => setUsername(target.value)}
+              />
+        </div>
+        <div>
+            password
+              <input
+              type="text"
+              value={password}
+              name="Password"
+              onChange={({ target }) => setPassword(target.value)}
+              />
+        </div>
+        <button type="submit">login</button>
+      </form>
+      </div>
+      )
+    }
+
+    return (
+      <div>
+      <h2>blogs</h2>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+      <button onClick={handleLogOut}>Log Out</button>
+    </div>
+  )
+}
+
+export default App
